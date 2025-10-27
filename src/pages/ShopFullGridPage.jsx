f33c47bea32e59d9906ecb02d1dc1041c7a2fa43
RAHULDAS6009 <das206053@gmail.com>
2025-10-27 12:15:19 +0530

    first commit

--- src/pages/ShopFullGridPage.jsx
+++ src/pages/ShopFullGridPage.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import ShopMainArea from "../components/ShopMainArea";
 
 const ShopFullGridPage = () => {
+  useEffect(() => {
+    console.log("hello");
+  }, []);
   return (
     <div>
       <Header />
@@ -15,7 +18,7 @@ const ShopFullGridPage = () => {
           </div>
           <ol className="breadcrumb">
             <li className="home">
-              <a title="Go to Home Page" href="index.html">
+              <a title="Go to Home Page" href="/">
                 Home
               </a>
             </li>
